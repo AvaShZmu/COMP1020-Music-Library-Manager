e6c4ba0764436cd5879b9f27c8ac319cdb6de80a
minhmoc207 <minhmoc1811@gmail.com>
2026-05-05 20:21:19 +0700

    Metadata extractor module done

--- src/main/java/module5/util/MetadataExtractor.java
+++ src/main/java/module5/util/MetadataExtractor.java
@@ -1,4 +1,52 @@
 package module5.util;
+import module1.audioModel.AudioItem;
+import java.io.File;
+
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.audio.AudioHeader;
+import org.jaudiotagger.tag.FieldKey;
+import org.jaudiotagger.tag.Tag;
 
 public class MetadataExtractor {
+    public static AudioItem extract(String filePath) {
+        try {
+            // jAudioTagger reads file
+            File audioFileObject = new File(filePath);
+            AudioFile audioFile = AudioFileIO.read(audioFileObject);
+
+            // Get ID3 Tags
+            Tag tag = audioFile.getTag();
+            String title = tag.getFirst(FieldKey.TITLE);
+            String artist = tag.getFirst(FieldKey.ARTIST);
+            String releaseDate = tag.getFirst(FieldKey.YEAR);
+            String genre = tag.getFirst(FieldKey.GENRE);
+
+            // Get Audiohead
+            AudioHeader audioHeader = audioFile.getAudioHeader();
+            int durationInSeconds = audioHeader.getTrackLength();
+
+            // Edge Case:
+            if (title == null || title.isEmpty()) {
+                title = audioFileObject.getName().replace(".mp3", "");
+            }
+
+            if (artist == null || artist.isEmpty()) {
+                artist = "Unknown Artist";
+            }
+
+            return new AudioItem(
+                    title,
+                    artist,
+                    releaseDate,
+                    durationInSeconds,
+                    genre,
+                    filePath
+            );
+        }
+        catch (Exception e) {
+            System.err.println("Error reading file: " + filePath);
+            return null;
+        }
+    }
 }
