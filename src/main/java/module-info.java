6c3b83461b362a598cb7621194b0e05728ed6d4f
Kha <doanduybaokha@gmail.com>
2026-05-06 19:40:23 +0700

    Add JavaFX project files

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module MusicLibraryApp {
+    requires com.google.gson;
+    requires jaudiotagger;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+}
